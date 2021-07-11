77d9b64d7f3a9ecbdc3ebef880a72a1fa2c7c666
Nischit Patel <47689523+nischitpatel@users.noreply.github.com>
2021-07-11 17:12:58 +0530

    Header file of BookShop class

---
+++ files/headerFiles/BookShop.h
@@ -0,0 +1,14 @@
+#ifndef BOOKSHOP_H
+#define BOOKSHOP_H
+
+class BookShop{
+	public:
+		void mainMenu();		//main menu
+		void addBook();			//to add new book in record
+		void dltBook();			//to delete book from record
+		void displayBooks();	//to see books in record
+		void updateBook();		//to update specific book record
+		void searchBook();		//to search aspecific book
+};
+
+#endif
